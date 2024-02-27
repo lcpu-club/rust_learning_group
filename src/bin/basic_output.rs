8ee8ee4168921af1e39ec7a49c9f53cd907ca3b2
忘忧北萱草 <wybxc@qq.com>
2024-02-27 11:08:36 +0800

    fix: markdown style

--- src/bin/basic_output.rs
+++ src/bin/basic_output.rs
@@ -5,7 +5,8 @@ use std::io::{self, Write};
 /// ### Output is just same logic as input!
 /// You may have learnt basic input. Now just fill up the code and expect
 /// `hey, stdout!` to be printed.
-/// ```
+///
+/// ```no_run
 /// fn stdout_lock() -> io::Result<()> {
 ///    // TODO: get stdout
 ///    /* FILL HERE */
@@ -15,16 +16,20 @@ use std::io::{self, Write};
 ///    Ok(())
 /// }
 /// ```
+///
 /// ### However, we have shortcuts: macro!
+///
 /// This is shorter but do the same thing!
-/// ```
+///
+/// ```no_run
 /// print!("hey, stdout!\n");
 /// ```
+///
 /// `println!` adds a '\n' automatically for you.
-/// ```
+///
+/// ```no_run
 /// println!("hey, stdout!");
 /// ```
-
 fn stdout_lock() -> io::Result<()> {
     todo!() // Paste code and complete it!
 }
--- src/bin/control_flow.rs
+++ src/bin/control_flow.rs
@@ -3,8 +3,10 @@
 use std::io;
 
 /// ### Control Flow if...else if...else...
+///
 /// Just like what you would write in C/C++:
-/// ```
+///
+/// ```cpp
 /// int x;
 /// scanf("%d", &x); // C
 /// std::cin >> x; // C++
@@ -19,14 +21,18 @@ use std::io;
 ///     std::cout << "The number is just 5!" << std::endl; // C++
 /// }
 /// ```
+///
 /// In Rust, just do it the same but remember to eliminate "()"!
-/// ```
+///
+/// ```no_run
 /// let x: i32;
 /// if x < 5 {
 ///     println!("The number is smaller than 5");
 /// }
 /// ```
+///
 /// Implement function cf_if which:
+///
 /// 1. take a number x (i32) from stdin.
 /// 2. Compare it with 5, print "Less", "Equal", "Greater" (remember newline)
 /// according to the result of comparing x with 5.
